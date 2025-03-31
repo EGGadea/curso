e9fb7367eabd2674cf67120387b81a36b0f15d04
EGGadea <esperanza.gadea.1992@gmail.com>
2025-03-31 17:53:14 +0000

    main

--- build.js
+++ build.js
@@ -1,7 +1,69 @@
-async function run() {
+import StyleDictionary from 'style-dictionary';
+import { register } from '@tokens-studio/sd-transforms';
+import ThemesLoader from 'sd-themes-loader';
+import { platform } from 'os';
+import { transform } from 'typescript';
 
 
-}
+register(StyleDictionary,{
+    withSDBuiltins: false,
+});
+
+const loader = ThemesLoader(StyleDictionary);
+
+async function run(params) {
+    const themes = await loader.load('/tokens');
+    const globalTheme = themes.getThemeByName('global');
+    const lightTheme = themes.getThemeByName ('light');
+
+    const globalConfig = {
+        expand: {
+            typesMap:true,
+        },
+        platforms: {
+            web: {
+                files: [
+                    {
+                        destination: 'app/build/global/variables.css',
+                        format: 'css/variables',
+                    }
+                ],
+                transforms:[
+                    'name/kebab',
+                    'ts/resolveMath',
+                    'ts/typography/fontWeight',
+                    'ts/size/lineheight',
+                    'size/pxToRem',
+                ]
+
+            }
+        }
+    };
+
+
+    const lightConfig = {
+        platforms: {
+            web: {
+                files: [
+                    {
+                        destination: 'app/build/light/variables.css',
+                        format: 'css/variables',
+
+                    }
+                ],
+
+                transforms: [
+                    'name/kebab',
+                    'color/rgb',
+
+                ]
+            }
+        }
+
+    };
+
+    globalTheme.addConfig(globalConfig).build();
+    lightTheme.addConfig(lightConfig).build();
 }
 
 run();
